8a556c2771a05492fd9a8cbe6a21afe068adf36f
Wojciech094 <WPLesniak@hotmail.com>
2025-11-20 09:14:34 +0100

    netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,13 @@
 import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
-	plugins: [tailwindcss()],
+	build: {
+		rollupOptions: {
+			input: {
+				main: './index.html',
+				login: './login.html',
+				register: './register.html',
+			},
+		},
+	},
 });
